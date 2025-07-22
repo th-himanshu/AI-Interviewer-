2f7fb24a1bfd206490944b28901dcfd70160cca7
Himanshu <agrawalh2021@gmail.com>
2025-07-22 23:21:47 +0530

    build without error again

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { UserButton } from '@clerk/nextjs';
 import React from 'react'
 import AddNewInterview from './_components/AddNewInterview';
-import Interviewlist from './_components/Interviewlist';
+import Interviewlist from './_components/InterviewList';
 const Dashboard = () => {
   return (
     <div className='p-10'>
